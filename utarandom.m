b206c57627a9beb3323bbf8b40117ccdc14994fc
Olivier Sobrie <olivier@sobrie.be>
2015-02-05 16:33:59 +0100

    utarandom: first ui value should be equal to 0

--- utarandom.m
+++ utarandom.m
@@ -13,6 +13,7 @@ xpts = zeros(ncriteria, nsegmax + 1);
 for i = 1:ncriteria
 	npts = nsegs(i) + 1;
 	xpts(i, 1:npts) = linspace(xdomains(i, 1), xdomains(i, 2), npts);
-	uis(i, 1:npts) = sort(rand(1, npts));
+	uis(i, 1) = 0;
+	uis(i, 2:npts) = sort(rand(1, npts - 1));
 	uis(i, 1:npts) = uis(i, 1:npts) ./ uis(i, npts) .* uimax(i);
 end
