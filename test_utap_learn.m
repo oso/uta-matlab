2705748e029a1c49a5c825be3c975f9f80fb5098
Olivier Sobrie <olivier@sobrie.be>
2015-02-06 15:02:35 +0100

    test_utap_learn: update plots

--- test_utap_learn.m
+++ test_utap_learn.m
@@ -3,63 +3,115 @@ close all; clear all; clc;
 % init pseudo-random number generator
 rand('seed', 123);
 
-na = 1000
+na = 100
 ncriteria = 5
 ncategories = 3
 
 % domains of the criteria
-xdomains = repmat([0 10], ncriteria, 1)
+xdomains = repmat([0 10], ncriteria, 1);
 
 % number of segments
-nsegs = repmat([4], ncriteria, 1)
+nsegs = repmat([10], ncriteria, 1);
 
 % generate random UTA functions
-[xpts, uis] = uta_random(xdomains, nsegs)
+[xpts, uis] = uta_random(xdomains, nsegs);
 
 % generate category thresholds
 %ucats = sort(rand(1, ncategories - 1))
 ucats = linspace(0, 1, ncategories + 1);
-ucats = ucats(2:ncategories)
+ucats = ucats(2:ncategories);
 
 % generate random performance table
-pt = pt_random(na, xdomains)
+pt = pt_random(na, xdomains);
 
 % compute assignments
 u = uta(xpts, uis, pt);
-assignments = utasort(ucats, u)
+assignments = utasort(ucats, u);
 
-% degree of the polynoms
-degree = 4;
+% degrees of the polynoms
+degrees = [3 4 5];
 
-% compute polynoms
-[pcoefs, ucats2] = utap_learn(degree, xdomains, ncategories, pt, assignments)
+results = cell(3);
 
-% plot UTA picewise linear functions and polynoms
+for i = 1:length(degrees)
+	deg = degrees(i)
+
+	% compute polynoms
+	[pcoefs, ucats2] = utap_learn(deg, xdomains, ncategories, pt, ...
+				      assignments);
+
+	% Check that umax is equal to 1
+	umax = 0;
+	for j = 1:ncriteria
+		umax = umax + polyval(pcoefs(j,:), xdomains(j, 2));
+	end
+	umax
+
+	% Assign the alternatives with the polynom
+	u2 = utap(pcoefs, pt);
+	assignments2 = utasort(ucats2, u2);
+
+	% Compute classification accuracy
+	assign_diff = assignments - assignments2;
+	errors = size(find(assign_diff > 0), 1);
+	ca = (na - errors) / na;
+
+	% Store ca, polynoms and category limits
+	results(i,1) = {pcoefs};
+	results(i,2) = {ucats2};
+	results(i,3) = {ca};
+end
+
+% plot UTA piecewise linear functions and polynoms
 figure
-for i = 1:ncriteria
-	subplot(2, ncriteria, i);
-	plot(xpts(i,:), uis(i,:), xpts(i,:), uis(i,:), 'b*');
+
+nsubx = 4;
+nsuby = ceil((ncriteria) / nsubx) + 1;
+%cmap = distinguishable_colors(length(degrees) + 1)
+cmap = hsv(length(degrees) + 1);
+plots = [];
+
+for j = 1:ncriteria
+	subplot(nsuby, nsubx, j);
+
+	hold on;
+	axis on;
+
+	ustr = sprintf('%g ', ucats2);
+	plotstr = sprintf('plinear;  U [%s]', ustr);
+
+	plots(1) = plot(xpts(j,:), uis(j,:), '-*', 'Color', cmap(1,:), ...
+			'DisplayName', plotstr);
+
+	hold off;
 end
 
-for i = 1:ncriteria
-	subplot(2, ncriteria, ncriteria + i);
-	x = xdomains(i,1):0.001:xdomains(i,2);
-	uval = polyval(pcoefs(i,:), x);
-	plot(x, uval);
+for i = 1:length(degrees)
+	deg = degrees(i);
+
+	for j = 1:ncriteria
+		subplot(nsuby, nsubx, j);
+
+		hold on;
+		axis on;
+
+		pcoefs = cell2mat(results(i, 1));
+		ucats2 = cell2mat(results(i, 2));
+		ca = cell2mat(results(i, 3));
+
+		x = xdomains(j,1):0.001:xdomains(j,2);
+		uval = polyval(pcoefs(j,:), x);
+
+		ustr = sprintf('%g ', ucats2);
+		plotstr = sprintf('degree %d; U [%s]; CA %g', deg, ustr, ca);
+
+		plots(i+1) = plot(x, uval, 'Color', cmap(i+1,:), ...
+				  'DisplayName', plotstr);
+
+		hold off;
+	end
 end
 
-% Check that umax is equal to 1
-umax = 0;
-for i = 1:ncriteria
-	umax = umax + polyval(pcoefs(i,:), xdomains(i, 2));
-end
-umax
-
-% Assign the alternatives with the polynom
-u2 = utap(pcoefs, pt);
-assignments2 = utasort(ucats2, u2)
-
-% Compute classification accuracy
-assign_diff = assignments - assignments2;
-errors = size(find(assign_diff > 0), 1);
-ca = (na - errors) / na
+sh = subplot(nsuby, nsubx, nsubx * nsuby);
+axis off;
+legend(sh, plots);
