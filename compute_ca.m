d19469411f6a03f3a68d071e634569a8d55ad9c9
Olivier Sobrie <olivier@sobrie.be>
2015-02-10 19:22:01 +0100

    compute_ca: fix error in ca computation

--- compute_ca.m
+++ compute_ca.m
@@ -1,6 +1,7 @@
 function ca = compute_ca(assignments, assignments2)
 
 na = length(assignments);
-assign_diff = assignments - assignments2;
+assign_diff = abs(assignments - assignments2);
 errors = size(find(assign_diff > 0), 1);
+length(errors)
 ca = (na - errors) / na;
