f16a678e6944a03bd6f1215d0a2a99c19e0029a3
Olivier Sobrie <olivier@sobrie.be>
2015-02-17 14:18:32 +0100

    test_utap_learn_from_csv_nseeds: check cvx_status before saving result

--- test_utap_learn_from_csv_nseeds.m
+++ test_utap_learn_from_csv_nseeds.m
@@ -17,11 +17,12 @@ ncriteria = size(pt_all, 2)
 %xdomains = repmat([0 1], ncriteria, 1);
 xdomains = [min(pt_all)' max(pt_all)'];
 
-results_learning = zeros(nseeds, length(degrees));
-results_test = zeros(nseeds, length(degrees));
+results_learning = cell(1, length(degrees));
+results_test = cell(1, length(degrees));
 for seed = 1:nseeds
-	seed
 	rand('seed', seed);
+
+	fprintf('seed: %d\n', seed);
 
 	na_learning = ceil(na * na_learning_pc / 100);
 	learning_indices = randi(na, na_learning, 1);
@@ -35,9 +36,16 @@ for seed = 1:nseeds
 		deg = degrees(i);
 
 		% compute polynomials
-		[pcoefs, ucats2] = utap_learn(deg, xdomains, ...
-					      ncategories, pt, ...
-					      assignments);
+		[pcoefs, ucats2, cvx_status] = utap_learn(deg, xdomains, ...
+							  ncategories, pt, ...
+							  assignments);
+
+		% check cvx status
+		k = strfind(cvx_status, 'Solved');
+		if length(k) < 1
+			fprintf('error: cvx_status: %s\n', cvx_status);
+			continue;
+		end
 
 		% Assign learning set and compute ca
 		u2 = utap(pcoefs, pt);
@@ -49,31 +57,29 @@ for seed = 1:nseeds
 		assignments2 = utasort(ucats2, u2);
 		ca_test = compute_ca(assignments_test, assignments2);
 
-		% Store ca, polynoms and category limits
-		results_learning(seed,i) = ca_learning;
-		results_test(seed,i) = ca_test;
+		% Store ca
+		results_learning(seed, i) = {ca_learning};
+		results_test(seed, i) = {ca_test};
 	end
 end
 
-results_learning
-results_test
-mean(results_learning)
-mean(results_test)
-
 fd = fopen(outfile, 'a+');
+for i = 1:length(degrees)
+	degree = degrees(i)
+	ca_learning = cell2mat(results_learning(:, i));
+	ca_test = cell2mat(results_test(:, i));
 
-fprintf(fd, '**********\n');
-fprintf(fd, 'pclearning %d\n', na_learning_pc);
-fprintf(fd, 'degree %d\n', degrees);
-fprintf(fd, '\nca learning set\n');
-dlmwrite(outfile, results_learning, '-append');
-fprintf(fd, '\nca test set\n');
-dlmwrite(outfile, results_test, '-append')
+	fprintf(fd, '*** degree: %d\n', degree);
+	fprintf(fd, 'pclearning %d\n', na_learning_pc);
+	fprintf(fd, '\nca learning set\n');
+	dlmwrite(outfile, ca_learning, '-append');
+	fprintf(fd, '\nca test set\n');
+	dlmwrite(outfile, ca_test, '-append')
 
-fprintf(fd, '\nca learning average\n');
-dlmwrite(outfile, mean(results_learning), '-append')
-fprintf(fd, '\nca test average\n');
-dlmwrite(outfile, mean(results_test), '-append')
-fprintf(fd, '\n\n');
-
+	fprintf(fd, '\nca learning average %g +- %g\n', ...
+		mean(ca_learning), std(ca_learning));
+	fprintf(fd, '\nca test average %g +- %g\n', ...
+		mean(ca_test), std(ca_test));
+	fprintf(fd, '\n\n');
+end
 fclose(fd);
