feb113f1190f320ff35fdd03d69d0390030f2b36
Olivier Sobrie <olivier@sobrie.be>
2015-02-17 14:16:34 +0100

    save_poly_utilities: print columns titles

--- save_poly_utilities.m
+++ save_poly_utilities.m
@@ -2,7 +2,7 @@ function plotrefs = save_poly_utilities(filename, p, xdomains)
 
 ncriteria = size(p, 1);
 
-plot_values = []
+plot_values = [];
 for j = 1:ncriteria
 	x = linspace(xdomains(j,1), xdomains(j,2));
 	uval = polyval(p(j,:), x);
@@ -11,10 +11,14 @@ for j = 1:ncriteria
 	plot_values(:, 2*j) = uval';
 end
 
-delete(filename)
+delete(filename);
 fd = fopen(filename, 'a+');
-for j = 1:
 
+str = sprintf('x%d,u%d', 1, 1);
+for j = 2:ncriteria
+	str = sprintf('%s,x%d,u%d', str, j, j);
+end
+fprintf(fd, '%s\n', str);
 dlmwrite(filename, plot_values, '-append');
 fprintf(fd, '\n');
 fclose(fd);
