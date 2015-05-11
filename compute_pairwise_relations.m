23e4dc0d9f215cbe617d5b7e68e640e9fcfe0d7f
Olivier Sobrie <olivier@sobrie.be>
2015-05-11 19:10:22 +0200

    compute_pairwise_relations: function to compute (min) pairwise relation
    This function compute the relations defining completely a
    ranking.

---
+++ compute_pairwise_relations.m
@@ -0,0 +1,18 @@
+function [pairwisecmp] = compute_pairwise_relations(u)
+
+[usorted, index] = sortrows(u, 1);
+na = length(u)
+
+pairwisecmp = [zeros(1, 3)];
+
+for i = 1:(na - 1)
+	if usorted(i + 1) > usorted(i)
+		pcmp = [index(i + 1), index(i), 1];
+	elseif usorted(i + 1) == usorted(i)
+		pcmp = [index(i + 1), index(i), 0];
+	elseif usorted(i + 1) < usorted(i)
+		pcmp = [index(i + 1), index(i), -1];
+	end
+
+	pairwisecmp(i, :) = pcmp;
+end
