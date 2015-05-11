d8a97eeb851a72c07f2af5ab1dad4f3aa34ddeee
Olivier Sobrie <olivier@sobrie.be>
2015-05-11 21:18:35 +0200

    test_computing_time: test the computing time of utap instead of utadisp

--- test_computing_time.m
+++ test_computing_time.m
@@ -3,39 +3,37 @@ ncriteria = 5
 ncat = 2
 deg = 5
 
+niterations = 100
+
 xdomains = repmat([-1 1], ncriteria, 1);
 nsegs = repmat([10], ncriteria, 1);
 
 % generate random UTA functions
 [xpts, uis] = uta_random(xdomains, nsegs);
 
-% generate category thresholds
-%ucats = sort(rand(1, ncategories - 1))
-ucats = linspace(0, 1, ncategories + 1);
-ucats = ucats(2:ncategories);
-
-t = [];
+tavg = [];
 for ina = 1:length(nas)
 	na = nas(ina)
 
-	% generate random performance table
-	pt = pt_random(na, xdomains);
+	t = [];
+	for i = 1:niterations
+		% generate random performance table
+		pt = pt_random(na, xdomains);
 
-	% compute assignments
-	u = uta(xpts, uis, pt);
-	assignments = utasort(ucats, u);
+		% compute utilities
+		u = uta(xpts, uis, pt);
+		pairwisecmp = compute_pairwise_relations(u);
 
-	% compute polynoms
-	tic;
-	[pcoefs, ucats2] = utadisp_learn(deg, xdomains, ncategories, ...
-					 pt, assignments);
-	t(ina) = toc;
+		% compute polynoms
+		tic;
+		[pcoefs] = utap_learn(deg, xdomains, pt, pairwisecmp);
+		t(i) = toc;
+	end
 
-	% print computing time
+	tavg(ina) = mean(t);
+	tstd(ina) = std(t);
 end
 
 for ina = 1:length(nas)
-	na = nas(ina);
-	tp = t(ina);
-	fprintf('%d %g\n', na, tp);
+	fprintf('%d\t%g\t%g\n', nas(ina), tavg(ina), tstd(ina));
 end
