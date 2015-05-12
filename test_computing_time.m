4efeae183c48902e9f92d595def03a49b06e5807
Olivier Sobrie <olivier@sobrie.be>
2015-05-12 21:44:04 +0200

    test_computing_time: create a function and do specific tests

--- test_computing_time.m
+++ test_computing_time.m
@@ -1,39 +1,6 @@
-nas = 50:50:500
-ncriteria = 5
-ncat = 2
-deg = 5
-
-niterations = 100
-
-xdomains = repmat([-1 1], ncriteria, 1);
-nsegs = repmat([10], ncriteria, 1);
-
-% generate random UTA functions
-[xpts, uis] = uta_random(xdomains, nsegs);
-
-tavg = [];
-for ina = 1:length(nas)
-	na = nas(ina)
-
-	t = [];
-	for i = 1:niterations
-		% generate random performance table
-		pt = pt_random(na, xdomains);
-
-		% compute utilities
-		u = uta(xpts, uis, pt);
-		pairwisecmp = compute_pairwise_relations(u);
-
-		% compute polynoms
-		tic;
-		[pcoefs] = utap_learn(deg, xdomains, pt, pairwisecmp);
-		t(i) = toc;
-	end
-
-	tavg(ina) = mean(t);
-	tstd(ina) = std(t);
-end
-
-for ina = 1:length(nas)
-	fprintf('%d\t%g\t%g\n', nas(ina), tavg(ina), tstd(ina));
-end
+test_uta_computing_time(50:50:100, 5,  3, 10, 'ctime')
+test_uta_computing_time(50:50:100, 7,  3, 10, 'ctime')
+test_uta_computing_time(50:50:100, 10, 3, 10, 'ctime')
+test_uta_computing_time(50:50:100, 5,  3, 10, 'ctime')
+test_uta_computing_time(50:50:100, 5,  4, 10, 'ctime')
+test_uta_computing_time(50:50:100, 5,  5, 10, 'ctime')
---
+++ test_uta_computing_time.m
@@ -0,0 +1,48 @@
+function test_uta_computing_time(nas, ncriteria, deg, nseg, fileprefix)
+
+niterations = 100
+
+xdomains = repmat([-1 1], ncriteria, 1);
+nsegs = repmat([nseg], ncriteria, 1);
+
+% generate random UTA functions
+[xpts, uis] = uta_random(xdomains, nsegs);
+
+tavg = [];
+for ina = 1:length(nas)
+	na = nas(ina)
+
+	t = [];
+	t2 = [];
+	for i = 1:niterations
+		% generate random performance table
+		pt = pt_random(na, xdomains);
+
+		% compute utilities
+		u = uta(xpts, uis, pt);
+		pairwisecmp = compute_pairwise_relations(u);
+
+		% compute polynomials
+		tic;
+		[pcoefs] = utap_learn(deg, xdomains, pt, pairwisecmp);
+		t(i) = toc;
+
+		% compute piecewise linear functions
+		tic;
+		[uis] = uta_learn(nsegs, xdomains, pt, pairwisecmp);
+		t2(i) = toc;
+	end
+
+	tavg(ina) = mean(t);
+	tstd(ina) = std(t);
+	t2avg(ina) = mean(t2);
+	t2std(ina) = std(t2);
+end
+
+[tavg' tstd' t2avg' t2std']
+d = dataset({[tavg' tstd' t2avg' t2std'] ...
+	     'UTAP_AVG' 'UTAP_STD' 'UTA_AVG' 'UTA_STD'})
+
+
+filename = sprintf('%s-%d-%d.dat', fileprefix, ncriteria, deg);
+export(d, 'File', filename, 'delimiter', ',')
