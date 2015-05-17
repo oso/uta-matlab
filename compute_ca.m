53a7727d4e015dc1c97eb2d03866c930f1246f20
Olivier Sobrie <olivier@sobrie.be>
2015-05-17 18:29:34 +0200

    compute_ca: be less verbose

--- compute_ca.m
+++ compute_ca.m
@@ -3,5 +3,4 @@ function ca = compute_ca(assignments, assignments2)
 na = length(assignments);
 assign_diff = abs(assignments - assignments2);
 errors = size(find(assign_diff > 0), 1);
-length(errors)
 ca = (na - errors) / na;
