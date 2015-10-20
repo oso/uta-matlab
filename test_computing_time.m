1df1ec53de3ed0c0349046303113c4d0d21bfb75
Olivier Sobrie <olivier@sobrie.be>
2015-10-20 18:22:29 +0200

    test_computing_time: update test

--- test_computing_time.m
+++ test_computing_time.m
@@ -1,4 +1,8 @@
 clear;
 test_utap_perfs([50:50:500], [5, 10], 'UTAP', [3 7], 10, 'ctime-exp1.txt')
+clear;
 test_utap_perfs([200, 500], [3:10], 'UTAP', [3 7], 10, 'ctime-exp2.txt')
+clear;
 test_utap_perfs([200, 500], [5, 10], 'UTAP', [3:7], 10, 'ctime-exp3.txt')
+clear;
+test_utap_perfs([200, 500], [5, 10], 'UTA', [3:10], 10, 'ctime-exp4.txt')
