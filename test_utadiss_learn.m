1283239ab2a39b48a0d923436de7a5de3637255a
Olivier Sobrie <olivier@sobrie.be>
2015-10-21 14:11:35 +0200

    utadiss_learn: add utadis-spline learning method

---
+++ test_utadiss_learn.m
@@ -0,0 +1,134 @@
+close all; clear all; clc;
+
+% cvx precision
+% cvx_precision best
+
+% init pseudo-random number generator
+rand('seed', 12);
+
+na = 100
+ncriteria = 5
+ncategories = 7
+
+% domains of the criteria
+xdomains = repmat([0 1], ncriteria, 1);
+
+% number of segments
+nsegs = repmat([10], ncriteria, 1);
+
+% generate random UTA functions
+[xpts, uis] = uta_random(xdomains, nsegs);
+
+% generate category thresholds
+%ucats = sort(rand(1, ncategories - 1))
+ucats = linspace(0, 1, ncategories + 1);
+ucats = ucats(2:ncategories);
+
+% generate random performance table
+pt = pt_random(na, xdomains);
+
+% compute assignments
+u = uta(xpts, uis, pt);
+assignments = utasort(ucats, u);
+
+% degrees of the polynomials
+nsegs2 = repmat([3], ncriteria, 1);
+xpts_splines = xlinspace(xdomains, nsegs2);
+degrees = [3];
+deg_continuity = 2;
+
+results = cell(1, 3);
+
+for i = 1:length(degrees)
+	deg = degrees(i)
+
+	% compute polynoms
+	[pcoefs, ucats2, cvx_status] = utadiss_learn(nsegs2, deg, ...
+						     deg_continuity, ...
+						     xdomains, ...
+						     ncategories, pt, ...
+						     assignments);
+	pcoefs
+
+	% check cvx status
+	k = strfind(cvx_status, 'Solved');
+	if length(k) < 1
+		fprintf('error: cvx_status: %s\n', cvx_status);
+	end
+
+	% Check that umax is equal to 1
+	umax = 0;
+	for j = 1:ncriteria
+		umax = umax + polyval(pcoefs(j,:,nsegs2(j)), xdomains(j, 2));
+	end
+	umax
+
+	% Assign the alternatives with the polynom
+	u2 = utas(xpts_splines, pcoefs, pt);
+	assignments2 = utasort(ucats2, u2);
+
+	% Compute classification accuracy
+	ca = compute_ca(assignments, assignments2);
+
+	% Store ca, polynoms and category limits
+	results(i,1) = {pcoefs};
+	results(i,2) = {ucats2};
+	results(i,3) = {ca};
+end
+
+% plot UTA piecewise linear functions and polynoms
+figure
+
+nplotsperline = 4;
+nlines = ceil((ncriteria) / nplotsperline) + 1;
+%cmap = distinguishable_colors(length(degrees) + 1)
+cmap = hsv(length(degrees) + 1);
+plots = [];
+
+ustr = sprintf('%g', ucats);
+plotstr = sprintf('plinear;  U [%s]', ustr);
+plotrefs = plot_pl_utilities(xpts, uis, '-*', cmap(1,:), plotstr, ...
+			     nplotsperline);
+plots(1) = plotrefs(1);
+
+% print piecewise linear functions
+fprintf('piecewise linear functions\n');
+fprintf('==========================\n\n');
+for j = 1:ncriteria
+	str = print_pl(xpts(j,:), uis(j,:));
+	fprintf('u_%d: %s\n', j, str);
+	fprintf('U: %s\n', ustr);
+end
+
+for i = 1:length(degrees)
+	deg = degrees(i);
+
+	pcoefs = cell2mat(results(i, 1));
+	ucats2 = cell2mat(results(i, 2));
+	ca = cell2mat(results(i, 3));
+
+	ustr = sprintf('%g ', ucats2);
+	plotstr = sprintf('degree %d; U [%s]; CA %g', deg, ustr, ca);
+
+	plotrefs = plot_poly_utilities(pcoefs, xdomains, '-', ...
+				       cmap(i+1,:), plotstr, ...
+				       nplotsperline);
+	plots(i + 1) = plotrefs(1);
+
+	% print degree
+	fprintf('\nDegree %d\n', deg);
+	fprintf('========\n\n');
+
+	% print uvals
+	fprintf('U: %s\n', ustr);
+
+	% print CA
+	fprintf('CA: %g\n', ca);
+end
+
+sh = subplot(nlines, nplotsperline, nplotsperline * nlines);
+axis off;
+legend(sh, plots);
+sh = subplot(nlines, nplotsperline, nplotsperline * nlines - 2);
+axis off;
+legend(sh, plots);
---
+++ utadiss_learn.m
@@ -0,0 +1,100 @@
+function [pcoefs, ucats, cvx_status] = utadiss_learn(nsegments, deg, ...
+						     deg_continuity, ...
+						     xdomains, ...
+						     ncategories, pt, ...
+						     assignments)
+
+epsilon = 0.00001;
+na = size(pt, 1);
+ncriteria = size(pt, 2);
+
+nsegmax = max(nsegments);
+xpts = zeros(ncriteria, nsegmax + 1);
+for i = 1:ncriteria
+	npts = nsegments(i) + 1;
+	xpts(i, 1:npts) = linspace(xdomains(i, 1), xdomains(i, 2), npts);
+end
+
+cvx_begin
+	variable a(deg + 1, ncriteria, nsegmax);
+	variable Q(deg, deg, ncriteria, nsegmax) symmetric;
+	variable ucats(ncategories - 1) nonnegative;
+	variable aplus(na) nonnegative;
+	variable amin(na) nonnegative;
+
+	minimize (sum(aplus) + sum(amin));
+	subject to
+		for i = 1:na
+			ap = pt(i, :);
+			assign = assignments(i);
+
+			u = 0;
+			for j = 1:ncriteria
+				for k = 2:nsegments(j) + 1
+					xpt = xpts(j, k);
+					if ap(j) <= xpt
+						break;
+					end
+				end
+
+				xpt2 = xpts(j, k - 1);
+				u = u + ap(j) .^ (0:deg) * a(:, j, k - 1);
+			end
+
+			if assign > 1
+				0 <= u - ucats(assign - 1) + aplus(i);
+			end
+
+			if assign < ncategories
+				-epsilon >= u - ucats(assign) - amin(i);
+			end
+		end
+
+		for j = 1:ncriteria
+			for k = 1:nsegmax
+				Q(:, :, j) == semidefinite(deg);
+			end
+		end
+
+		Q(:,:,:,:) <= 10;
+		Q(:,:,:,:) >= -10;
+
+		for i = 1:ncategories-2
+			ucats(i) <= ucats(i + 1);
+		end
+
+		ucats(ncategories - 1) <= 1;
+
+		for j = 1:ncriteria
+			xdomains(j, 1).^(0:deg)*a(:, j, 1) == 0;
+		end
+
+		umax = 0;
+		for j = 1:ncriteria
+			umax = umax + xdomains(j, 2).^(0:deg)*a(:, j, nsegments(j));
+		end
+		umax == 1;
+
+
+		for d = 0:deg_continuity
+			z = [];
+			for l = 1+d:size(a(:, i))
+				z(l - d) = factorial(l - 1) / factorial(l - 1 - d);
+			end
+
+			for j = 1:ncriteria
+				for i = 2:nsegments(j)
+					x1 = z .* xpts(j, i) .^ (0:deg-d) * a(d + 1:deg + 1, j, i);
+					x2 = z .* xpts(j, i) .^ (0:deg-d) * a(d + 1:deg + 1, j, i - 1);
+
+					x1 == x2;
+				end
+			end
+		end
+cvx_end
+
+for k = 1:nsegmax
+	pcoefs(:,:,k) = fliplr(a(:,:,k)');
+end
+
+pcoefs;
