45d02f21e30360146b1badc290ecfda47cc6d536
Olivier Sobrie <olivier@sobrie.be>
2015-10-25 16:16:14 +0100

    print_splines_utilities: comment some lines

--- print_splines_utilities.m
+++ print_splines_utilities.m
@@ -2,14 +2,14 @@ function print_splines_utilities(p, xpts, file)
 
 ncriteria = size(p, 1);
 
-results = []
+results = [];
 for j = 1:ncriteria
-	xs = []
-	uval = []
+	xs = [];
+	uval = [];
 	for k = 1:size(xpts, 2)-1
 		x = linspace(xpts(j, k), xpts(j, k + 1));
 		uval = [uval polyval(p(j,:,k), x)];
-		xs = [xs; x']
+		xs = [xs; x'];
 	end
 
 	results = [results xs uval'];
