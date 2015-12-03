fd26dc17eb1e21f0a7d6371789e0171f339157fd
Olivier Sobrie <olivier@sobrie.be>
2015-12-03 20:34:14 +0100

    utadis_learn{2}: return xpts in output

--- test_utadiss_learn.m
+++ test_utadiss_learn.m
@@ -33,7 +33,6 @@ assignments = utasort(ucats, u);
 
 % degrees of the polynomials
 nsegs2 = repmat([10], ncriteria, 1);
-xpts_splines = xlinspace(xdomains, nsegs2);
 degrees = [3];
 deg_continuity = 2;
 
@@ -43,12 +42,10 @@ for i = 1:length(degrees)
 	deg = degrees(i)
 
 	% compute polynoms
-	[pcoefs, ucats2, cvx_status] = utadiss_learn2(nsegs2, deg, ...
-						      deg_continuity, ...
-						      xdomains, ...
-						      ncategories, pt, ...
-						      assignments);
-	pcoefs
+	[xpts_splines, pcoefs, ucats2, cvx_status] = ...
+			utadiss_learn2(nsegs2, deg, deg_continuity, ...
+				       xdomains, ncategories, pt, ...
+				       assignments);
 
 	% check cvx status
 	k = strfind(cvx_status, 'Solved');
