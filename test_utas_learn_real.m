dbc3318467ad7482f7f0894f057a27417d5456bf
Olivier Sobrie <olivier@sobrie.be>
2016-01-22 18:33:24 +0100

    test_utas_learn_real: make it more generic, use model functions

--- test_utas_learn_real.m
+++ test_utas_learn_real.m
@@ -1,40 +1,21 @@
 close all; clear all; clc;
 
-rand('seed', 123);
+rand('seed', 1);
 
-na = 50;
+na = 100;
+nagen = 1000;
+model = @m2u;
 
 xdomains = [0 1; 0 1; 0 1];
 
-% generate 3 polynomials
-x = [0 0.5 1];
-y = [0 0.1 0.4]
-f1 = polyfit(x, y, 2);
-f1 = f1 ./ polyval(f1, 1) * 0.4
-
-x = [0:0.01:0.1 0.3:0.01:0.5 0.7:0.01:1];
-y = [0:0.002:0.02 0.19:0.001:0.21 0.38:0.001:0.41];
-f2 = polyfit(x, y, 15);
-f2 = f2 ./ polyval(f2, 1) * 0.4
-
-x = [0 0.5 1]
-y = [0 0.15 0.2]
-f3 = polyfit(x, y, 2);
-f3 = f3 ./ polyval(f3, 1) * 0.2
-
-uf = zeros(3, 16);
-uf(1,17-length(f1):16) = f1;
-uf(2,17-length(f2):16) = f2;
-uf(3,17-length(f3):16) = f3;
-
 % generate performance table and pairwise comparisons
 pt = pt_random(na, xdomains);
-u = utap(uf, pt);
+u = model(pt);
 pairwisecmp = compute_pairwise_relations(u);
 
 % parameters of uta-splines
 nsegments = 3
-degree = 5
+degree = 4
 continuity = 2
 
 % learn marginal utilities
@@ -54,12 +35,22 @@ ranking2 = compute_ranking(u2);
 spearmand = compute_spearman_distance(ranking, ranking2)
 kendallt = compute_kendall_tau(ranking, ranking2)
 
+rand('seed', 456);
+pt = pt_random(nagen, xdomains);
+u = model(pt);
+u2 = utas(xpts, pcoefs, pt);
+ranking = compute_ranking(u);
+ranking2 = compute_ranking(u2);
+
+spearmand = compute_spearman_distance(ranking, ranking2)
+kendallt = compute_kendall_tau(ranking, ranking2)
+
 % plot the marginals
 cmap = hsv(2);
 plots = [];
 
 plotstr = 'real';
-plotrefs = plot_poly_utilities(uf, xdomains, '-', cmap(1,:), plotstr, 3);
+plotrefs = plot_model_utilities(model, xdomains, '-', cmap(1,:), plotstr, 3);
 plots(1) = plotrefs(1);
 
 plotstr = sprintf('nsegs: %d; deg: %d; cont: %d; SD: %g; KT: %g', ...
@@ -70,3 +61,5 @@ plots(2) = plotrefs(1);
 sh = subplot(2, 3, 5);
 axis off;
 legend(sh, plots);
+
+print_splines_utilities(pcoefs, xpts, 'spline.dat')
